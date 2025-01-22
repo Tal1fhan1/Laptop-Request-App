3a6574d5d44a1788aa77f2010c70c3f38c3336e4
Tal1fhan1 <talifhaninetshiheni@gmail.com>
2025-01-22 15:49:42 +0200

    Allowed Hosts

--- asset-request-app/asset_request_app/asset_request_app/settings.py
+++ asset-request-app/asset_request_app/asset_request_app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['talifhani.pythonanywhere.com']
 
 LOGIN_URL = 'login'
 
